8fe7f7b643ec39e679d5252b3202880b234a4b5a
Tim <tbarry2@andrew.cmu.edu>
2021-08-06 14:30:00 -0400

    call lib(ondisc) in try_odm

--- try_odm.R
+++ try_odm.R
@@ -1,4 +1,5 @@
 gasp_offsite <- .get_config_path("LOCAL_GASPERINI_2019_DATA_DIR")
 odm_fp <- paste0(gasp_offsite, "at-scale/processed/gene_expressions")
-odm <- ondisc::read_odm(odm_fp = odm_fp)
+library(ondisc)
+odm <- read_odm(odm_fp = odm_fp)
 odm[[,1]]

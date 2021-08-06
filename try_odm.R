8ced5915cd2ba461134099feb870a76b3d78f280
Tim <tbarry2@andrew.cmu.edu>
2021-08-06 15:04:07 -0400

    rm try_odm; update .gitignore

--- try_odm.R
+++
@@ -1,5 +0,0 @@
-gasp_offsite <- .get_config_path("LOCAL_GASPERINI_2019_DATA_DIR")
-odm_fp <- paste0(gasp_offsite, "at-scale/processed/gene_expressions")
-library(ondisc)
-odm <- read_odm(odm_fp = odm_fp)
-odm[[,1]]
